30fad4292dd48a6c8cb4f95a8553556faec6c1b3
HeerKaisair <1767407822@qq.com>
2021-08-07 11:54:44 +0800

    结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,32 @@
 module.exports = {
-    title: 'Amiable Doc',
-    description: 'A C# SDK For Bot(Almost All Bot Platform) Plugin Development',
-    base:"/Amiable-Doc/"
+  title: 'Amiable Doc',
+  description: 'A C# SDK For Bot(Almost All Bot Platform) Plugin Development',
+  base: "/Amiable-Doc/",
+  themeConfig: {
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: '概述',
+        link: '/summary/'
+      },
+      {
+        text: '指南',
+        link: '/guide/'
+      },
+      {
+        text: 'API与事件',
+        link: '/api/'
+      },
+      {
+        text: '更多',
+        link: '/more/'
+      },
+      {
+        text: '往昔的记忆',
+        link: '/updatelogs/'
+      },
+    ]
   }
+}
--- docs/README.md
+++ docs/README.md
@@ -3,6 +3,7 @@ home: true
 heroText: Amiable
 tagline: 一个多框架兼容的C# Bot开发SDK
 actionText: 快速上手 →
+actionLink: /summary/
 
 features:
 - title: 佛系维护
@@ -10,10 +11,5 @@ features:
 - title: 多次迭代沉淀
   details: 先驱C#SDK的多次更新换代造就了Amiable
 - title: 开源与社区
-  details: 一个和谐的Bot爱好者社区 botfan.cn
+  details: Github就当作SDK的社区好了~
 ---
-# Amiable是什么
-Amiable是一个C# Bot插件开发SDK，设计模式上采用适配器模式，以便兼容大部分(几乎所有)市面上的框架。
-
-# Amiable收费吗
-用爱发电的赫尔从不收费，当然你可以尝试去[爱发电](http://afdian.net/@heerkaisair)赞助赫尔。
