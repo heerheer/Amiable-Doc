51e6015e25517d0791782ac41c7079f0d1ccc485
HeerKaisair <1767407822@qq.com>
2021-08-07 14:20:52 +0800

    md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: 'A C# SDK For Bot(Almost All Bot Platform) Plugin Development',
   base: "/Amiable-Doc/",
   themeConfig: {
+    displayAllHeaders: true,
     nav: [{
         text: 'Home',
         link: '/'
@@ -27,6 +28,40 @@ module.exports = {
         text: '往昔的记忆',
         link: '/updatelogs/'
       },
-    ]
+    ],
+    sidebar: {
+      "/summary/": [
+        "简介",
+        "目标开发者"
+      ],
+      '/guide/': [{
+          title: '基础',
+          collapsable: false,
+          children: [
+            '快速上手'
+          ]
+        }, {
+          title: '进阶',
+          collapsable: false,
+          children: [
+            '目录结构',
+            '基本配置',
+            '多框架兼容',
+            '编译',
+          ]
+        },
+        // {
+        //   title: '骚操作',
+        //   collapsable: false,
+        //   children: [
+        //     '多个插件放置在同一解决方案内',
+        //     '使用MsgRoute',
+        //     '使用AmiConfig过滤授权群聊',
+        //     '改写APIWrapper增加API',
+        //   ]
+        // }
+
+      ]
+    }
   }
 }
